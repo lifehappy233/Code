0ed156d55f30b855d6354edc0fef0898b1bd6d03
root <858953471@qq.com>
2022-02-23 22:22:06 +0800

    20220223

---
+++ C++/B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  vector<int> vt;
+  for (int i = 1; i <= 10; i++) {
+    vt.push_back(i);
+  }
+  for (auto it : vt) {
+    cout << it << endl;
+  }
+  return 0;
+}
